108fc37525a099be85354852475d6e7addca41bd
MaheshDoiphode <58901842+MaheshDoiphode@users.noreply.github.com>
2022-10-02 10:21:10 +0530

    Delete 28.FindtheIndexoftheFirstOccurrenceinaString.java

--- 28.FindtheIndexoftheFirstOccurrenceinaString.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
-         if (needle.isEmpty()) {
-            return 0;
-        }
-        return haystack.indexOf(needle);
-    }
-}
