0514c4b6452b9672c4381d2e8ead2d15f77a1c5c
Namgup001 <51751217+Namgup001@users.noreply.github.com>
2022-10-02 10:44:50 +0530

    Create 242. Valid Anagram.cpp
    please add it under the label hacktoberfest-2022

---
+++ C++/242. Valid Anagram.cpp
@@ -0,0 +1,33 @@
+
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+Solution-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        unordered_map<char, int> counts;
+        for (int i = 0; i < n; i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for (auto count : counts)
+            if (count.second) return false;
+        return true;
+    }
+};
